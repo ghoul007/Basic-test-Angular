35754f985ff2f565dec21fac7e63dcebf3b2453e
ahmed GHOUL <ghoul.ahmed5@gmail.com>
2020-03-22 16:34:37 +0000

    simple example

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { UserService } from "./user.service";
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+  providers: [UserService, DataService]
+})
+export class UserComponent implements OnInit {
+  user: {name: string};
+  isLoggedIn = false;
+  data: string;
+
+  constructor(private userService: UserService, private dataService: DataService) { }
+
+  ngOnInit() {
+    this.user = this.userService.user;
+    this.dataService.getDetails().then((data: string) => this.data = data);
+  }
+
+}
